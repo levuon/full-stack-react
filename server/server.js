a6ca9de23d34b287fad44efd52a8e5b38395a38b
lev <levuonliu@gmail.com>
2017-06-26 11:05:07 +0800

    cache

--- server/server.js
+++ server/server.js
@@ -19,6 +19,19 @@ app.use(bodyParser.json({
   extended: false
 }));
 
+const cache = [
+  {
+    articleId: '987654',
+    articleTitle: 'javascript - artical one',
+    articleContent: 'lalalala i am javascript from mongodb'
+  },
+  {
+    articleId: '123456',
+    articleTitle: 'web - artical two',
+    articleContent: 'en i am web from mongodb'
+  }
+]
+
 const model = new falcor.Model({
   'cache': cache
 });
